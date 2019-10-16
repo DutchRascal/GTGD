cd47732a6edade5cb0d2dde28c3f82fd18c09627
DutchRascal <DutchRascal@fritz.box>
2019-10-16 17:39:15 +0200

    Cleaned Standard Assets 20191016-1730

---
+++ Assets/GTGD 3/Scripts/Welcome.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Chapter1
+{
+    public class Welcome : MonoBehaviour
+    {
+        private string myString = "Welcome!";
+        private Text textWelcome;
+
+        private void Start()
+        {
+            SetInitiaReferences();
+            MyWelcomeMessage();
+        }
+
+        void SetInitiaReferences()
+        {
+            textWelcome = GameObject.Find("TextWelcome").GetComponent<Text>();
+        }
+
+        void MyWelcomeMessage()
+        {
+            if (textWelcome != null)
+            {
+                textWelcome.text = myString;
+            }
+            else
+            {
+                Debug.Log("welcomeText not assigned!");
+            }
+        }
+    }
+}
