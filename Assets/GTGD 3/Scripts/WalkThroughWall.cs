f6e49c5c39550159aa7e8b4f9acaf9c1dfd081bc
DutchRascal <DutchRascal@fritz.box>
2019-10-19 17:38:37 +0200

    20191018-1730-12

---
+++ Assets/GTGD 3/Scripts/WalkThroughWall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter1
+{
+    public class WalkThroughWall : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            gameObject.layer = LayerMask.NameToLayer("Not Solid");
+        }
+
+        private void OnDisable()
+        {
+            gameObject.layer = LayerMask.NameToLayer("Default");
+        }
+    }
+}
