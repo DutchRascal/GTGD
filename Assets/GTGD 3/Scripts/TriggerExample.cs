6784a1d85184ca9359cfea4b786b153f34d5167b
DutchRascal <DutchRascal@fritz.box>
2019-10-19 17:51:43 +0200

    20191018-1750-13

---
+++ Assets/GTGD 3/Scripts/TriggerExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter1
+{
+    public class TriggerExample : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(other.name + " has entered");
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            Debug.Log(other.name + " has exited");
+
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            Debug.Log(other.name + " is in trigger");
+        }
+    }
+}
