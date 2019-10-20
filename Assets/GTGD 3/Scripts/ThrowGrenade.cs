350fdafb9d07d88e53ba500a8759b275a10e960f
DutchRascal <DutchRascal@fritz.box>
2019-10-20 17:25:41 +0200

    20191019-1730-16

---
+++ Assets/GTGD 3/Scripts/ThrowGrenade.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter1
+{
+    public class ThrowGrenade : MonoBehaviour
+    {
+        public GameObject grenade;
+        private Transform myTransform;
+
+        void Start()
+        {
+            SetInitialReferences();
+        }
+
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                SpawnGrenade();
+            }
+        }
+
+        void SetInitialReferences()
+        {
+            myTransform = transform;
+        }
+
+        void SpawnGrenade()
+        {
+            GameObject gameObject = Instantiate(grenade, myTransform.TransformPoint(0, 0, 0.5f), myTransform.rotation);
+            Destroy(gameObject, 10f);
+        }
+    }
+}
