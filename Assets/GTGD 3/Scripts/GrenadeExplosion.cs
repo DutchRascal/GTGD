76dc13341f75ee4c830e088bcd2814f9ed39dc9b
DutchRascal <DutchRascal@fritz.box>
2019-10-23 18:00:18 +0200

    20191023-1800-18

---
+++ Assets/GTGD 3/Scripts/GrenadeExplosion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter1
+{
+    public class GrenadeExplosion : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log(collision.contacts[0].point.ToString());
+            Destroy(gameObject);
+        }
+    }
+}
